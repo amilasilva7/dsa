b0530fab8c245eb294a3511eb0a8ba12920e17dd
amilasilva7 <osapsilva@gmail.com>
2026-02-02 19:08:00 +0530

    DSA: Find index of first occurrence in a String

---
+++ src/test/java/org/ostech/dsa/chanllanges/IndexOfFirstOccurrenceTest.java
@@ -0,0 +1,19 @@
+package org.ostech.dsa.chanllanges;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IndexOfFirstOccurrenceTest {
+
+    @Test
+    void indexOfFirstOccurrence() {
+        assertEquals(0, IndexOfFirstOccurrence.indexOfFirstOccurrence("sadbutsad", "sad"));
+        assertEquals(-1, IndexOfFirstOccurrence.indexOfFirstOccurrence("leetcode", "leeto"));
+        assertEquals(-1, IndexOfFirstOccurrence.indexOfFirstOccurrence("", "sad"));
+        assertEquals(-1, IndexOfFirstOccurrence.indexOfFirstOccurrence("leetcode", ""));
+        assertEquals(-1, IndexOfFirstOccurrence.indexOfFirstOccurrence("", ""));
+        assertEquals(0, IndexOfFirstOccurrence.indexOfFirstOccurrence("a", "a"));
+    }
+
+}
